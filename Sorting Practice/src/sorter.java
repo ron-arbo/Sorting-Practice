9af067980612fb111ef69a2ebc6e8a31959366bc
ron_arbo <ron.arbo63@icloud.com>
2019-01-08 20:23:36 -0500

    Committing bubbleSort and logistic methods

--- Sorting Practice/src/sorter.java
+++ Sorting Practice/src/sorter.java
@@ -1,4 +1,34 @@
+import java.util.List;
 
 public class sorter {
 
+	public sorter(){
+		
+	}
+	
+	public void printList(List<Integer> toPrint) {
+		for(Integer i: toPrint) {
+			System.out.print(toPrint.get(i) + " ");
+		}
+	}
+	
+	public List<Integer> bubbleSort(List<Integer> unsorted){
+		for(int out = unsorted.size()-1; out > 0; out--) {
+			for(int in = 0; in < out; in++) {
+				if(unsorted.get(in) > unsorted.get(in + 1)) {
+					swap(unsorted, in, in+1);
+				}
+			}
+		}
+		return unsorted;
+	}
+	
+	//Swaps two values in a list (parameter is the index)
+	public void swap(List<Integer> list, int first, int second) {
+		Integer temp = list.get(first);
+		list.set(first, list.get(second));
+		list.set(second, temp);
+	}
+	
+	
 }
